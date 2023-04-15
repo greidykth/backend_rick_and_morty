e6e92f2b929b7073e141b02d9243e5a0286edfe4
Greidy Peña <greidykp@gmail.com>
2023-04-15 12:03:02 -0500

    Fix exclude .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config(); //para usar las variables de entorno
 const server = require('./src/server');
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
