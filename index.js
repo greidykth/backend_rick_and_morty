85f06a1f1550811975094b0ef155c2ba6f866d43
Greidy Peña <greidykp@gmail.com>
2023-04-21 15:13:11 -0500

    Add db connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config(); //para usar las variables de entorno
 const server = require('./src/server');
-
+const { conn } = require('./src/DB_connection');
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
+conn.sync({force:true}).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+})
+.catch((err) => console.log(err.message));
