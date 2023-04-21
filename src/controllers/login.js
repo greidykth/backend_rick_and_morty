52e8baa8da5cbbaf11d0f144c115e4b66cbadce8
Greidy Peña <greidykp@gmail.com>
2023-04-21 17:16:21 -0500

    Fix login controller

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,26 +1,28 @@
-const validUsers = require("../utils/users");
+const { User } = require("../DB_connection");
 
 const STATUS_OK = 200;
-const STATUS_ERROR = 404;
+const STATUS_ERROR = 500;
 
-const login = (req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.query;
 
   try {
     if (!password || !email) {
-      return res
-        .status(500)
-        .json({ message: "There isn't a password or email" });
+      res.status(400).json({ message: "Password or email missing" });
     }
-    const findUser = validUsers.find(
-      (user) => user.email === email && user.password === password
-    );
 
-    if (findUser) {
-      res.status(STATUS_OK).json({ access: true });
-    } else {
-      res.status(STATUS_OK).json({ access: false });
+    const findUser = await User.findOne({ where: { email: email }});
+
+    if (!findUser) {
+      res.status(404).json({ message: "User not found" });
     }
+
+    if (findUser.password === password) {
+      res.status(200).json({ access: true });
+    } else {
+      res.status(403).json({ error: "Invalid password" });
+    }
+
   } catch (error) {
     res.status(STATUS_ERROR).json({ error: error.message });
   }
