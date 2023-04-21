bed88961458ef01a502dabb4643dbdbb5dbef398
Greidy Peña <greidykp@gmail.com>
2023-04-21 17:15:28 -0500

    Fix routes

--- src/routes/rickandMortyRoutes.js
+++ src/routes/rickandMortyRoutes.js
@@ -1,12 +1,15 @@
 const {Router} = require('express');
 const getCharById = require('../controllers/getCharById');
 const login = require('../controllers/login');
-const { postFav, deleteFav } = require('../controllers/handleFavorites');
+const postUser = require('../controllers/postUser');
+const postFav = require('../controllers/postFav');
+const deleteFav = require('../controllers/deleteFav');
 
 const routes = Router();
 
 routes.get('/character/:id', getCharById);
 routes.get("/login", login);
+routes.post("/login", postUser);
 routes.post("/fav", postFav);
 routes.delete("/fav/:id", deleteFav);
 
